0b375edf5a500ed217f2788488a4ae9cc7cd91fe
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-17 02:15:11 +0300

    maket start

---
+++ src/Home/Hero.js
@@ -0,0 +1,22 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import SplitButton from 'react-bootstrap/SplitButton';
+
+function MenuAlignResponsiveExample() {
+    return (
+        <>
+            <div className='bg-danger' >
+                <div className='container'>
+                    <div className='hero'>
+                        <span className='d-block  hero__leni '>Chase the new Flavour</span>
+                        <h1 className='text-white hero__title ' >The key to Fine dining</h1>
+                        <p className='text-white hero__text ' >Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                        <button className='text-white hero__btn bg-transparent' >Explore Menu</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MenuAlignResponsiveExample;
---
+++ src/Home/Home.js
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from './imges/logo.svg'
+function BasicExample() {
+    return (
+        <Navbar bg="danger" expand="md" >
+            <Container>
+                <Navbar.Brand href="#home"><img src={logo} /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link className='text-white' href="#home">Menu</Nav.Link>
+                        <Nav.Link className='text-white' href="#link">News</Nav.Link>
+                        <Nav.Link className='text-white' href="#link">About Us</Nav.Link>
+                        <Nav.Link className='text-white' href="#link">Contact Us</Nav.Link>
+                        <Nav.Link className='text-white border rounded-pill border-light border-2 px-3' href="#link"> Log in </Nav.Link>
+                      
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default BasicExample;
