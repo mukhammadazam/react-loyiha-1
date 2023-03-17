f35346f3c53c6c9914392410be51c77f948d4051
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-18 01:33:45 +0300

    news start

--- src/Home/News.js
+++ src/Home/News.js
@@ -1,9 +1,19 @@
+import newsImg from './imges/news-img.png';
 const newsFun = () => {
     return (
         <>
             <div className="container">
-                <div className="row">
 
+                <div className="news">
+                    <p className="news__text text-center">News</p>
+                    <h3 className="news__title text-center">Gerícht updates</h3>
+                    <div className="row">
+                        <div className='col-12 col-sm-12 col-lg-4 news__item'>
+                            <img src={newsImg} />
+                            <h4 className='news__item--title'>tips for prepping and caring for your grill</h4>
+                            <p className='news__item--text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
