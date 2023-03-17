de66f16afc167c5227d0aae91d3b7c1f871fd9ae
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-18 02:38:53 +0300

    news.js finished

--- src/Home/News.js
+++ src/Home/News.js
@@ -7,14 +7,27 @@ const newsFun = () => {
                 <div className="news">
                     <p className="news__text text-center">News</p>
                     <h3 className="news__title text-center">Gerícht updates</h3>
-                    <div className="row justify-content-lg-between justify-content-center">
+                    <div className="row news__row justify-content-lg-between justify-content-center">
                         <div className='col-12 p-0 col-sm-12 col-lg-4 news__item'>
-                            <img className='p-0' src={newsImg} />
+                            <div className='d-flex align-items-center justify-content-center'>  <img className='p-0 news__img' src={newsImg} /></div>
                             <h4 className='news__item--title pt-3 px-4'>Tips for prepping and caring for your grill</h4>
                             <p className='news__item--text pt-1 px-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
-                            <button className='news__btn ps-4'>Read More</button>
+                            <div className='news__wrapper-btn d-flex align-items-center justify-content-between ps-4 pe-5'><button className='news__btn '>Read More</button><p className='news__btn--text'>16 Apr 2021</p></div>
+                        </div>
+                        <div className='col-12 p-0 col-sm-12 col-lg-4 news__item'>
+                            <div className='d-flex align-items-center justify-content-center'>  <img className='p-0 news__img' src={newsImg} /></div>
+                            <h4 className='news__item--title pt-3 px-4'>Tips for prepping and caring for your grill</h4>
+                            <p className='news__item--text pt-1 px-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                            <div className='news__wrapper-btn d-flex align-items-center justify-content-between ps-4 pe-5'><button className='news__btn '>Read More</button><p className='news__btn--text'>16 Apr 2021</p></div>
+                        </div>
+                        <div className='col-12 p-0 col-sm-12 col-lg-4 news__item'>
+                            <div className='d-flex align-items-center justify-content-center'>  <img className='p-0 news__img' src={newsImg} /></div>
+                            <h4 className='news__item--title pt-3 px-4'>Tips for prepping and caring for your grill</h4>
+                            <p className='news__item--text pt-1 px-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.</p>
+                            <div className='news__wrapper-btn d-flex align-items-center justify-content-between ps-4 pe-5'><button className='news__btn '>Read More</button><p className='news__btn--text'>16 Apr 2021</p></div>
                         </div>
                     </div>
+                    <button className='news__button d-block'>View More</button>
                 </div>
             </div>
         </>
