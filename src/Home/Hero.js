122835fedd906a84f95ca015374dcad0907d7d13
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-17 04:02:16 +0300

    hero desctop finished

--- src/Home/Hero.js
+++ src/Home/Hero.js
@@ -6,14 +6,14 @@ function MenuAlignResponsiveExample() {
         <>
             <div className='bg-danger' >
                 <div className='container'>
-                    <div className='hero-site d-flex align-items-start justify-content-between'>
+                    <div className='hero-site d-flex  justify-content-center  align-items-start justify-content-between'>
                         <div className='hero'>
-                            <span className='d-block text-white  hero__leni '>Chase the new Flavour</span>
-                            <h1 className='text-white hero__title ' >The key to Fine dining</h1>
-                            <p className='text-white hero__text ' >Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-                            <button className='text-white hero__btn bg-transparent' >Explore Menu</button>
+                            <span className='d-block text-white   hero__leni '>Chase the new Flavour</span>
+                            <h1 className='text-white hero__title  ' >The key to Fine dining</h1>
+                            <p className='text-white hero__text  ' >Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                            <button className='hero__btn ' >Explore Menu</button>
                         </div>
-                        <div className='pt-5 '> <img className='pt-5' src={shashlik} /></div>
+                        <div className='pt-5 '> <img className='pt-5 hero__img position-relative' src={shashlik} /></div>
                     </div>
                 </div>
             </div>
