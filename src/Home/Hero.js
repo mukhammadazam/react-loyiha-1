bca55edb66865c26fa7aaa39b5f28e5519eba611
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-17 04:25:53 +0300

    hero tablet start

--- src/Home/Hero.js
+++ src/Home/Hero.js
@@ -5,15 +5,15 @@ function MenuAlignResponsiveExample() {
     return (
         <>
             <div className='bg-danger' >
-                <div className='container'>
+                <div className='container d-flex align-items-center justify-content-center'>
                     <div className='hero-site d-flex  justify-content-center  align-items-start justify-content-between'>
                         <div className='hero'>
-                            <span className='d-block text-white   hero__leni '>Chase the new Flavour</span>
-                            <h1 className='text-white hero__title  ' >The key to Fine dining</h1>
-                            <p className='text-white hero__text  ' >Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-                            <button className='hero__btn ' >Explore Menu</button>
+                            <span className='d-block text-white  text-center hero__leni '>Chase the new Flavour</span>
+                            <h1 className='text-white hero__title text-center ' >The key to Fine dining</h1>
+                            <p className='text-white hero__text text-center ' >Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                            <button className='hero__btn d-block ' >Explore Menu</button>
                         </div>
-                        <div className='pt-5 '> <img className='pt-5 hero__img position-relative' src={shashlik} /></div>
+                        <div className='pt-5 '> <img className='pt-5 hero__img d-none d-lg-block position-relative' src={shashlik} /></div>
                     </div>
                 </div>
             </div>
