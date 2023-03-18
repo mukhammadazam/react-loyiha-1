0e59ef707a170751d8dd6bc5779b46ee76853d94
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-03-18 09:59:22 +0300

    Footer.js i am working on it

--- src/Home/Footer.js
+++ src/Home/Footer.js
@@ -2,17 +2,20 @@
 
 const foterFun = () => {
     return (
-        <>
-        <div className="container">
-            <div className="Newsletter">
-                <p className="Newsletter__text">Newsletter</p>
-                <p className="Newsletter__title">Subscribe to Our Newsletter</p>
-                <div>
-                    <input type={"email"} placeholder="Email Address" />
+        <div className="letter-site">
+            <div className="container">
+                <div className="letter">
+                    <p className="letter__text text-center">Newsletter</p>
+                    <p className="letter__title text-center">Subscribe to Our Newsletter</p>
+                    <form className="d-flex align-items-center justify-content-center gap-5 ">
+                        <input className="d-block letter__input text-white" type={"email"} placeholder="Email Address" />
+                        <button className="letter__btn">Subscribe</button>
+                    </form>
+                    <div className="contact"></div>
                 </div>
             </div>
+
         </div>
-        </>
     );
 }
 
